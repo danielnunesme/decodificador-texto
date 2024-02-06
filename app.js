5b5e772f88ca4008ef03004bd5ed642f7dc96963
Daniel Nunes <nunes.dnt@gmail.com>
2024-02-05 21:19:21 -0300

    Ajustes botao copiar, funçao validarTexto

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ var inputTexto = document.querySelector('#input-texto');
 var mensagemCriptH1 = 'A mensagem foi encriptada.';
 var mensagemCriptSpan = 'Clique em descriptografar para a mensagem voltar ao normal.';
 var mensagemDescripth1 = 'A mensagem foi descriptada.';
-var mensagemDescriptSpan = 'Clique em criptografar caso queira encriptá-la novamente.'
+var mensagemDescriptSpan = 'Clique em criptografar caso queira encriptá-la novamente.';
+var areaTransferencia = 'Texto copiado para área de transferência!';
+var erroAreaTrannsferencia = 'Erro ao copiar para a área de transferência: ';
+var textoCriptografia;
 
+inputTexto.addEventListener('input', validarTexto);
+
+function validarTexto() {
+    var texto = inputTexto.value;
+    // Expressão regular para verificar se há letras maiúsculas ou caracteres especiais
+    var regex = /[A-Z]|[^a-zA-Z\s]/;
+    var botaoCriptografar = document.getElementById('criptografar');
+    if (regex.test(texto)) {
+        // Se a expressão regular encontrar correspondências, o texto é inválido
+        botaoCriptografar.disabled = true; 
+    } else {
+        // Se não houver correspondências, o texto é válido
+        botaoCriptografar.disabled = false;
+    }
+}
 
 function criptografar() {
     var textoCript = inputTexto.value;
@@ -12,10 +30,11 @@ function criptografar() {
     .replace(/a/g, "ai")
     .replace(/o/g, "ober")
     .replace(/u/g, "ufat");
-    document.getElementById('output').innerHTML = '<textarea id="texto-secreto" readonly>' + textoCriptografia + '</textarea>' + '<button onclick="copiar()" class="botao-copiar">Copiar</button>';
+    inputTexto.value = textoCriptografia;
     document.getElementById("mensagemH1").innerHTML = mensagemCriptH1;
     document.getElementById("mensagemSpam").innerHTML = mensagemCriptSpan;
     document.getElementById('descrip').removeAttribute('disabled');
+    document.getElementById('copiar').style.display = 'block';
 }
 
 function descriptografar() {
@@ -25,20 +44,21 @@ function descriptografar() {
       .replace(/ai/g, "a")
       .replace(/ober/g, "o")
       .replace(/ufat/g, "u");
-      document.getElementById('output').innerHTML = '<textarea id="texto-secreto" readonly>' + textoCriptografia + '</textarea>' + '<button onclick="copiar()" class="botao-copiar">Copiar</button>';
-      document.getElementById("mensagemH1").innerHTML = mensagemDescriptH1;
+      inputTexto.value = textoCriptografia;
+      document.getElementById("mensagemH1").innerHTML = mensagemDescripth1;
       document.getElementById("mensagemSpam").innerHTML = mensagemDescriptSpan;
       document.getElementById('descrip').setAttribute('disabled',true);
+      
   }
 
 function copiar() {
     const resultText = textoCriptografia;
     navigator.clipboard.writeText(resultText)
-    .then(()=> {
-        alert("Texto copiado para área de transferência!")
+    .then(() => {
+        document.getElementById("areaTransferencia").innerText = areaTransferencia;
     })
     .catch(err => {
-        console.error('Erro ao copiar para a área de transferência: ', err);
+        document.getElementById("areaTransferencia").innerHTML = erroAreaTrannsferencia;
     });
     
 }
